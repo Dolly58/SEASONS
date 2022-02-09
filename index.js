4b0bf95fd70ee577ea2ac095de7f44561b06369c
Dolly58 <asdfg60931@gmail.com>
2022-02-09 12:12:28 +0800

    AAA

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict"
+
+/*********** 網頁高度 ***********/
+window.addEventListener("scroll", function(e){
+    console.log("scrollX: " + window.scrollX);
+    console.log("scrollY: " + window.scrollY);
+  });
+
+  1640
+
