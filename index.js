ff85828868e3806a16bc6fca22c34077c24d0972
Dolly58 <asdfg60931@gmail.com>
2022-02-13 10:30:49 +0800

    js_hidetogglebtn

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 "use strict"
 
 /*********** 網頁高度 ***********/
-window.addEventListener("scroll", function(e){
-    console.log("scrollX: " + window.scrollX);
-    console.log("scrollY: " + window.scrollY);
-  });
+// window.addEventListener("scroll", function(e){
+//     console.log("scrollX: " + window.scrollX);
+//     console.log("scrollY: " + window.scrollY);
+//   });
 
 /*********** 執行AOS ***********/
   // AOS.init();
   
-/*********** 展開收合***********/
-$(document).ready(function(){
-  $(".hidetogglebtn").click(function(){
-  $(this).next(".showcontent").slideToggle();
+/*********** 單品頁展開收合***********/
+// $(document).ready(function(){
+//   $(".hidetogglebtn").click(function(){
+//   $(this).next(".showcontent").slideToggle();
+//   });
+// });
+
+var btn1 = document.getElementsByClassName("hidetogglebtn");
+var i;
+
+for (i = 0; i < btn1.length; i++) {
+  btn1[i].addEventListener("click", function() {
+  this.classList.toggle("showcontent");
+  var showcontent = this.nextElementSibling;
+  if (showcontent.style.display === "block") {
+    showcontent.style.display = "none";
+  } else {
+    showcontent.style.display = "block";
+  }
   });
-});
-
-
-
+}
