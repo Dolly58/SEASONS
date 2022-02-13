60391f83da8e7c36c919000bf6f21dfc2ec44e06
Dolly58 <asdfg60931@gmail.com>
2022-02-13 10:36:28 +0800

    slideToggle_animation

--- index.js
+++ index.js
@@ -8,23 +8,23 @@
 
   
 /*********** 單品頁展開收合***********/
-// $(document).ready(function(){
-//   $(".hidetogglebtn").click(function(){
-//   $(this).next(".showcontent").slideToggle();
-//   });
-// });
-
-var btn1 = document.getElementsByClassName("hidetogglebtn");
-var i;
-
-for (i = 0; i < btn1.length; i++) {
-  btn1[i].addEventListener("click", function() {
-  this.classList.toggle("showcontent");
-  var showcontent = this.nextElementSibling;
-  if (showcontent.style.display === "block") {
-    showcontent.style.display = "none";
-  } else {
-    showcontent.style.display = "block";
-  }
+$(document).ready(function(){
+  $(".hidetogglebtn").click(function(){
+  $(this).next(".showcontent").slideToggle();
   });
-}
+});
+
+// var btn1 = document.getElementsByClassName("hidetogglebtn");
+// var i;
+
+// for (i = 0; i < btn1.length; i++) {
+//   btn1[i].addEventListener("click", function() {
+//   this.classList.toggle("showcontent");
+//   var showcontent = this.nextElementSibling;
+//   if (showcontent.style.display === "block") {
+//     showcontent.style.display = "none";
+//   } else {
+//     showcontent.style.display = "block";
+//   }
+//   });
+// }
