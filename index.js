d56c944f11db171bec1830bd270f41d419a976cf
Dolly58 <asdfg60931@gmail.com>
2022-02-19 15:54:33 +0800

    slick

--- index.js
+++ index.js
@@ -82,3 +82,14 @@ $(function(){
   
 
 });
+
+// 圖片輪播
+$(document).ready(function(){
+$('.slick_fade').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+});
