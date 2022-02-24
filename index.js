2722f4a19605395f7d3c9d44017692c1c5ef0f6c
Dolly58 <asdfg60931@gmail.com>
2022-02-24 15:56:42 +0800

    LOADING

--- index.js
+++ index.js
@@ -132,3 +132,11 @@ $(document).ready(function(){
 //   popup.classList.add("noshow")
 //   mask.classList.remove("mask")
 // }
+
+// loading gif
+// $(window).load(function() { // 確認整個頁面讀取完畢再將這三個div隱藏起來
+//   $("#status").delay(5000).fadeOut(3000); //delay --> 延遲幾秒才fadeOut
+//   $("#preloader").delay(7000).fadeOut(1500);
+// })
+
+
