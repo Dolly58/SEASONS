bd741d6a0bf1d216396b07d2da0373a328b0a5d2
Dolly58 <asdfg60931@gmail.com>
2022-02-28 11:08:51 +0800

    add news.js

--- news.js
+++ news.js
@@ -3,3 +3,18 @@ let mask = document.getElementsByClassName('news_bg')[0];
 let Xbtn = document.getElementById('close');
 let box = document.getElementById('box');
 
+box.onclick = function show(){
+    popup.classList.remove("noshow")
+    mask.classList.add("mask")
+    }
+
+    Xbtn.onclick  = function close(){
+    popup.classList.add("noshow")
+    mask.classList.remove("mask")
+    }
+
+    mask.onclick  = function close(){
+    popup.classList.add("noshow")
+    mask.classList.remove("mask")
+    }
+
